a8a765ce81c87276bc4ec963a4c9e7874594d148
Kassia Fraga <kassiafraga7@gmail.com>
2022-04-07 14:25:08 -0300

    .

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,7 @@ import Home from "../pages/Home";
 
 export default function AppRoutes (){
     return (
-        <Router basename={process.env.REACT_APP_BASENAME}>
+        <Router>
             <Switch>
                 <Route exact path="/">
                     <Home />
