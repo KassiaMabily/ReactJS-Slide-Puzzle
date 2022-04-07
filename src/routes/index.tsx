062cf8673434711fd1ebd26605cbfe7d5b81280d
Kassia Fraga <kassiafraga7@gmail.com>
2022-04-07 14:32:12 -0300

    .

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,15 @@
 import {
     BrowserRouter as Router,
     Switch,
-    Route
+    Route,
+    HashRouter
 } from "react-router-dom";
 import Game from "../pages/Game";
 import Home from "../pages/Home";
 
 export default function AppRoutes (){
     return (
-        <Router basename={process.env.PUBLIC_URL}>
+        <HashRouter basename={process.env.PUBLIC_URL}>
             <Switch>
                 <Route exact path="/">
                     <Home />
@@ -17,6 +18,6 @@ export default function AppRoutes (){
                     <Game />
                 </Route>
             </Switch>
-        </Router>
+        </HashRouter>
     )
 }
